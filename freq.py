2c629dfb87b62caeed2068cac4658ab3f0738ef1
Mike Gleen <mike.gleen@gmail.com>
2026-08-16 08:41:54 +0100

    ckpt

--- freq.py
+++ freq.py
@@ -18,23 +18,27 @@ def index_of_coincidence(tally):
             numerator += count * (count - 1)
         total += count
     ic = numerator / (total * (total - 1))
-    print(ic)
+    print(f'IC = {ic:6.3}')
 
-cipherfile = open(sys.argv[1])
-cipher = cipherfile.read()
-print(cipher)
-tally = defaultdict(int)
-# print(tally)
-for ch in cipher:
-    if ch.isalpha():
-        tally[ch.upper()] += 1
-print(sorted(tally.items(), key=lambda item: item[1], reverse=True))
+def main():
+    cipherfile = open(sys.argv[1])
+    cipher = cipherfile.read()
+    print(cipher)
+    tally = defaultdict(int)
+    # print(tally)
+    for ch in cipher:
+        if ch.isalpha():
+            tally[ch.upper()] += 1
+    print(sorted(tally.items(), key=lambda item: item[1], reverse=True))
 
-# print only uppercase letters
-# outs = []
-# for ch in cipher:
-#     if not ch.isalpha() or ch == ch.upper():
-#         outs.append(ch)
-# print(''.join(outs))
+    # print only uppercase letters
+    # outs = []
+    # for ch in cipher:
+    #     if not ch.isalpha() or ch == ch.upper():
+    #         outs.append(ch)
+    # print(''.join(outs))
 
-index_of_coincidence(tally)
+    index_of_coincidence(tally)
+
+if __name__ == '__main__':
+    main()
